007ed6fac2654ea26101480b77e496965c42e70f
Tadayoshi Sato <tadayosi@gmail.com>
2014-03-07 18:05:28 +0900

    Shell sort

--- src/test/scala/algorithms/sort/SortTest.scala
+++ src/test/scala/algorithms/sort/SortTest.scala
@@ -8,7 +8,7 @@ import scala.util.Random
 class SortTest {
 
     private val ascend = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-    private val descend = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    private val descend = Array(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
     @Test
     def selection {
@@ -21,6 +21,14 @@ class SortTest {
     @Test
     def insertion {
         val s = new InsertionSort[Int]
+        test(s, ascend)
+        test(s, descend)
+        for (_ <- 1 to 10) test(s, randomArray(10))
+    }
+
+    @Test
+    def shell {
+        val s = new ShellSort[Int]
         test(s, ascend)
         test(s, descend)
         for (_ <- 1 to 10) test(s, randomArray(10))
